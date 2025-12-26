ecc6533f1460c2afa2c3a873c3e46ca63ab70eb2
Rammah Karpous <rammahkarpous@outlook.com>
2025-12-26 14:08:39 +0100

    feat: create authentication feature with sanctum and create api endpoints

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) 
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        Auth::login($user);
+
+        return response()->json([
+            'user' => $user,
+            'message' => 'Registered successfully'
+        ], 200);
+    }
+
+    public function login(Request $request) 
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ( ! Auth::attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect'],
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'user' => Auth::user(),
+            'message' => 'Login successful'
+        ]);
+    }
+
+    public function logout(Request $request) {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+
+    public function user(Request $request) 
+    {
+        return response()->json($request->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', [AuthController::class, 'user'])->name('auth.user');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
